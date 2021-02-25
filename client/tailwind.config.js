0fd6f2eb009b56221a311af67e0126c06f6ff6fc
kevinadhiguna <kevin26798@gmail.com>
2021-02-26 05:02:46 +0700

    chore: modify purge

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
