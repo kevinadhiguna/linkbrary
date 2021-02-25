187234ff9404cf30fe83c45503a4b2280be338f4
kevinadhiguna <kevin26798@gmail.com>
2021-02-26 04:43:03 +0700

    refactor: import Tailwind CSS

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ApolloProvider } from '@apollo/client';
 import { AppProps } from 'next/app';
 import { useApollo } from '../lib/apolloClient';
+import "tailwindcss/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
     const apolloClient = useApollo(pageProps);
